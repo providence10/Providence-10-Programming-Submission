c5e8e9a9758b7d4f23f358ec70ca953d2088afa7
providence10 <49004693+providence10@users.noreply.github.com>
2019-03-27 14:10:34 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,219 @@
+#include <kipr/botball.h>
+
+int main()
+{
+   
+    motor(0, 80);
+    motor(1, -80);
+    msleep(1200);
+        
+    ao();
+    
+    motor(0, 80);
+    motor(1, 80);
+    msleep(1600);
+        
+    ao();
+    
+    motor(0, 80);
+    motor(1, -80);
+    msleep(5600);
+        
+    ao();
+    
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1500);
+    
+    ao();
+    
+    motor(0, 80);
+    motor(1, -80);
+    msleep(2000);
+    
+    ao();
+    
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1850);
+    
+    ao();
+    
+    motor(0, 80);
+    motor(1, -80);
+    msleep(7050);
+    
+    ao();
+    
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1750);
+    
+    ao();
+     
+    motor(0, 80);
+    motor(1, -80);
+    msleep(2700);
+    
+    ao();
+    
+     
+    motor(0, -80);
+    motor(1, 80);
+    msleep(2700);
+    
+    ao();
+     
+    motor(0, 80);
+    motor(1, 80);
+    msleep(1475);
+    
+    ao();
+     
+    motor(0, 80);
+    motor(1, -80);
+    msleep(2750);
+    
+    ao();
+        
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1500);
+    
+    ao();
+        
+    motor(0, 80);
+    motor(1, -80);
+    msleep(2750);
+        
+    ao();
+       
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1550);
+    
+    ao();        
+   
+    motor(0, 80);
+    motor(1, -80);
+    msleep(1750);
+        
+    ao();
+           
+            
+    motor(0, -80);
+    motor(1, 80);
+    msleep(2300);
+        
+    ao();
+            
+    motor(0, -80);
+    motor(1, -80);
+    msleep(1850);
+        
+    ao();
+               
+    motor(0, 80);
+    motor(1, -80);
+    msleep(3070);
+        
+    ao();
+            
+    motor(0, 90);
+    motor(1, 80);
+    msleep(1150);
+        
+    ao();
+       
+    motor(0, 100);
+    motor(1, -100);
+    msleep(150);
+        
+    ao();
+      
+    motor(0, -100);
+    motor(1, -100);
+    msleep(390);
+        
+    ao();
+        
+    motor(0, 100);
+    motor(1, -100);
+    msleep(500);      
+    
+    ao();
+    
+    motor(0, 100);
+    motor(1, 100);
+    msleep(450);
+        
+    ao();
+      
+    motor(0, 80);
+    motor(1, -80);
+    msleep(5000);
+    
+    ao();
+    
+    motor(0, 100);
+    motor(1, 100);
+    msleep(225);
+         
+    ao();  
+    
+    motor(0, 100);
+    motor(1, -100);
+    msleep(200);
+    
+    ao();
+        
+    motor(0, -100);
+    motor(1, -100);
+    msleep(225);
+    
+    ao();
+      
+    motor(0, 100);
+    motor(1, -100);
+    msleep(300);
+    
+    ao();
+           
+    motor(0, 100);
+    motor(1, 100);
+    msleep(1000);
+    
+    ao();
+    
+    motor(0, 100);
+    motor(1, -100);
+    msleep(3070);
+    
+    ao();
+       
+    motor(0, 100);
+    motor(1, 100);
+    msleep(1600);
+    
+    ao();
+        
+    motor(0, 100);
+    motor(1, -100);
+    msleep(3250);
+    
+    ao();
+        
+    motor(0, -100);
+    motor(1, -100);
+    msleep(1200);
+    
+    ao();
+    
+    motor(0, 100);
+    motor(1, -100);
+    msleep(600);
+    
+    ao();
+       
+    return 0;
+}
